763ee72c1c6ccbad8dd8d3947527c3a3632dabe9
EDUARDO MENDOZA <rl238513@rlas-macair-36.local>
2018-11-14 11:07:01 -0600

    app icon

--- GradingCalculator/ViewController.swift
+++ GradingCalculator/ViewController.swift
@@ -15,29 +15,46 @@ class ViewController: UIViewController {
     @IBOutlet weak var currentGrade: UITextField!
     @IBOutlet weak var desiredGrade: UITextField!
     @IBOutlet weak var examWeight: UITextField!
+    @IBOutlet weak var image: UIImageView!
     
     
     
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        self.view.backgroundColor = .red
+        image.backgroundColor = .green
     }
     
     @IBAction func button(_ sender: Any) {
         
-         if let startingGrade = Double(currentGrade.text!), let endGrade = Double(desiredGrade.text!), let endGrade = Double(examWeight){
-        var grade = (100 * endGrade - (100 - finalExamWeight) * startingGrade) / finalExamWeight
-         } else {
+        currentGrade.resignFirstResponder()
+        desiredGrade.resignFirstResponder()
+        examWeight.resignFirstResponder()
+        acolor()
+        if let startingGrade = Double(currentGrade.text!), let endGrade = Double(desiredGrade.text!), let finalExamWeight = Double(examWeight.text!){
+            let grade = (100 * endGrade - (100 - finalExamWeight) * startingGrade) / finalExamWeight
+            numberGrade.text = String(grade)
+            
+            
+            
+            
+        } else {
             let alert = UIAlertController(title: "empty textfield", message: "good job dumb dumb want some gum gum", preferredStyle: .alert)
             alert.addAction(UIAlertAction(title: "ok", style: .default, handler: nil))
             self.present(alert, animated: true, completion: nil)
         }
-
-
-
-
-
-
-}
+        
+        
+        
+        
+        
+        
+    }
+    func acolor(){
+        if image.backgroundColor == .green{
+            image.backgroundColor = .red
+        }else{
+            image.backgroundColor = .green
+        }}
 }
